cf745e12964c55d6d0b4f5e3b0163535068d3620
gomadoufu <49393637+gomadoufu@users.noreply.github.com>
2025-05-02 18:52:10 +0900

    第4章終了 レイヤードアーキテクチャに書き換えた

--- rusty-book-manager/src/bin/app.rs
+++ rusty-book-manager/src/bin/app.rs
@@ -1,53 +1,31 @@
-use sqlx::{PgPool, postgres::PgConnectOptions};
 use std::net::{Ipv4Addr, SocketAddr};
 
-use anyhow::Result;
-use axum::{Router, extract::State, http::StatusCode, routing::get};
+use adapter::database::connect_database_with;
+use anyhow::{Error, Result};
+use api::route::health::build_health_check_routers;
+use axum::Router;
+use registry::AppRegistry;
+use shared::config::AppConfig;
 use tokio::net::TcpListener;
-
-pub async fn health_check() -> StatusCode {
-    StatusCode::OK
-}
-
-async fn health_check_db(State(db): State<PgPool>) -> StatusCode {
-    let connection_result = sqlx::query("SELECT 1").fetch_one(&db).await;
-    match connection_result {
-        Ok(_) => StatusCode::OK,
-        Err(_) => StatusCode::INTERNAL_SERVER_ERROR,
-    }
-}
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let database_cfg = DatabaseConfig {
-        host: "localhost".to_string(),
-        port: 5432,
-        username: "app".to_string(),
-        password: "passwd".to_string(),
-        database: "app".to_string(),
-    };
+    bootstrap().await
+}
 
-    let conn_pool = connect_database_with(database_cfg);
+async fn bootstrap() -> Result<()> {
+    let app_config = AppConfig::new()?;
+    let pool = connect_database_with(&app_config.database);
+    let registry = AppRegistry::new(pool);
 
     let app = Router::new()
-        .route("/health", get(health_check))
-        .route("/health/db", get(health_check_db))
-        .with_state(conn_pool);
+        .merge(build_health_check_routers())
+        .with_state(registry);
 
     let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
+    let listener = TcpListener::bind(addr).await?;
 
-    let listener = TcpListener::bind(&addr).await?;
-    Ok(axum::serve(listener, app).await?)
-}
+    println!("Listening on {}", addr);
 
-#[tokio::test]
-async fn health_check_works() {
-    let status_code = health_check().await;
-    assert_eq!(status_code, StatusCode::OK);
-}
-
-#[sqlx::test]
-async fn health_check_db_works(pool: sqlx::PgPool) {
-    let status_code = health_check_db(State(pool)).await;
-    assert_eq!(status_code, StatusCode::OK);
+    axum::serve(listener, app).await.map_err(Error::from)
 }
